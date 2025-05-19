9d28a18b8796bd05b337516fbb80ff37e43b4054
mohamadalinzm <mohamadalinzm@gmail.com>
2025-05-19 08:12:57 +0330

    Refactor API routes to use a versioned prefix and streamline task endpoints

--- routes/api.php
+++ routes/api.php
@@ -15,28 +15,27 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/v1/login', [LoginUserController::class, 'login']);
-Route::post('/v1/register', [RegisterUserController::class, 'register']);
-Route::middleware('auth:sanctum')->post('/v1/logout', [LogoutUserController::class, 'logout']);
+Route::prefix('v1')->group(function () {
+    Route::post('/login', [LoginUserController::class, 'login']);
+    Route::post('/register', [RegisterUserController::class, 'register']);
+    Route::middleware('auth:sanctum')->post('/logout', [LogoutUserController::class, 'logout']);
 
+    Route::middleware(['auth:sanctum', 'check.permission'])->group(function () {
+        Route::get('/tasks', [GetAllTaskController::class, 'index'])
+            ->name('api.tasks.index');
 
-Route::group(['middleware' => ['auth:sanctum','check.permission']], function () {
+        Route::post('/tasks', [CreateTaskController::class, 'store'])
+            ->name('api.tasks.create');
 
-    Route::get('/v1/tasks', [GetAllTaskController::class, 'index'])
-        ->name('api.tasks.index');
+        Route::get('/tasks/{task}', [FindTaskByIdController::class, 'show'])
+            ->name('api.tasks.show');
 
-    Route::post('/v1/tasks', [CreateTaskController::class, 'store'])
-        ->name('api.tasks.create');
+        Route::put('/tasks/{task}', [UpdateTaskController::class, 'update'])
+            ->name('api.tasks.update');
 
-    Route::get('/v1/tasks/{task}', [FindTaskByIdController::class, 'show'])
-        ->name('api.tasks.show');
-
-    Route::put('/v1/tasks/{task}', [UpdateTaskController::class, 'update'])
-        ->name('api.tasks.update');
-
-    Route::delete('/v1/tasks/{task}', [DeleteTaskController::class, 'destroy'])
-        ->name('api.tasks.destroy');
-
+        Route::delete('/tasks/{task}', [DeleteTaskController::class, 'destroy'])
+            ->name('api.tasks.destroy');
+    });
 });
 
 
